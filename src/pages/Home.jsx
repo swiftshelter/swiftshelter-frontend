982818e31d75ffe82f4189af78a365e64f04d3a2
rajiss-ctrl <omosanjos77#gmail.com>
2024-02-08 16:00:02 +0100

    sign in popo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NewestListing from '../components/NewestListing'
 import ExploreCitiesWrapper from '../components/ExploreCitiesWrapper'
 import Nav from '../components/Nav/nav'
 import Hero from '../components/Hero/hero'
+import UserAuth from '../components/UserAccess'
 
 
 
 const Home = () => {
+  const [showForm, setShowForm]= useState(false)
+
+  const handleVisiblity = ()=>{
+    setShowForm((pre)=>!pre)
+    console.log("Navbar")
+  }
   return (
-    <div>
-      <Nav />
+    <div className='home'>
+      <div className={showForm ? "hiddepop" : "user-auth"}>
+        <UserAuth showForm={showForm} handleVisiblity={handleVisiblity} />
+      </div>
+      <Nav handleVisiblity={handleVisiblity}/>
       <Hero />
       <div className="x-spacing">
         <NewestListing/>
